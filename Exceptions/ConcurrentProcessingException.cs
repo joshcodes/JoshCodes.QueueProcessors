9808cee6497337c13b910098cfc96653b9763fd5
Josh Wingstrom <josh@magicmoments.com>
2013-08-08 16:11:05 -0400

    initial

---
+++ Exceptions/ConcurrentProcessingException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoshCodes.QueueProcessors
+{
+    public class ConcurrentProcessingException : Exception
+    {
+        public ConcurrentProcessingException(string movingToState, string expectedState, string foundState, string message = null)
+        {
+        }
+    }
+}
